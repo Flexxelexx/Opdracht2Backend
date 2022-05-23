7b6eadaae6b2a5c18353dc0eb7b6ba500dfb15e1
Flexxelexx <alexxkooij@hotmail.com>
2022-05-23 09:35:43 +0200

    second draft

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main (String[] args) {
-
+        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+        applePieRecipe.verwarmOven();
+        applePieRecipe.klopEi();
+        applePieRecipe.mengAlles();
+        applePieRecipe.schilAppels();
+        applePieRecipe.vetSpringvorm();
+        applePieRecipe.vulEneHelft();
+        applePieRecipe.vulAndereHelft();
+        applePieRecipe.rolEnSnij();
+        applePieRecipe.decoreerTaart();
+        applePieRecipe.bakTaart();
+        applePieRecipe.printIngredient();
     }
+
 }
